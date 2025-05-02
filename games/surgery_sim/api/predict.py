007a3d814235802b7be7995343fc15a12a34f31d
Ishir Garg <gargishir@gmail.com>
2025-05-01 17:19:07 -0700

    untested part of api added

--- games/surgery_sim/api/predict.py
+++ games/surgery_sim/api/predict.py
@@ -2,11 +2,15 @@
 # https://cog.run/python
 
 from cog import BasePredictor, Input, Path
+import model
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
         # self.model = torch.load("./weights.pth")
+        self.device = model.get_device()
+        args = model.get_args()
+        self.model, self.tokenizer = model.load_model(args)
 
     def predict(
         self,
@@ -14,8 +18,10 @@ class Predictor(BasePredictor):
     ) -> dict:
         """Run a single prediction on the model"""
         # Run inference with the given neurons tweaked by the knob amount
-    
-        # Return the resulting prediction
-        prediction = "lebron"
+        input_strings = []
+        neuron_list_to_steer = []
+
+        token_id = 3
+        prediction = model.steer_token_activations_logits(self.model, self.tokenizer, input_strings, neuron_list_to_steer, token_id, self.device)
     
         return {"prediction": prediction}
